04f71a1553be08cc454a565dfec8db0b5f68831d
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-20 22:39:03 -0700

    Title screen is completed. Game instructions next. Then make it all play nice together.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// /* AUDIO */
+// const titleSong = document.getElementById("title-menu-song");
+// titleSong.src = document.createObjectURL(
+//   "assets/audio/8bit-title-menu-audio.mp3"
+// );
+// titleSong.load();
+// titleSong.play();
